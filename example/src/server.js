4a82a36f3eb59a24896837113ec305c17308d346
Szymon Maslowski <szymon.maslowski@grandparade.co.uk>
2019-07-04 22:13:01 +0200

    Add example usage

---
+++ example/src/server.js
@@ -0,0 +1,30 @@
+const { resolve } = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const watchModule = require('../../');
+const webpackConfig = require('../webpack.config');
+
+const ssrWatched = watchModule('./ssr.js', {
+  watch: ['./App.jsx'],
+  disable: process.env.NODE_ENV === 'production',
+});
+
+const port = process.argv[2];
+
+const app = express();
+const compiler = webpack(webpackConfig);
+app.use(require('morgan')('tiny'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(resolve('client')));
+} else {
+  app.use(webpackDevMiddleware(compiler, { stats: 'minimal' }));
+}
+app.use(/^\/$/, async (req, res) => {
+  const ssr = await ssrWatched.module;
+  const response = ssr({ title: 'Hello World' });
+  res.send(response);
+});
+
+app.listen(port, () => console.info(`Listening on port ${port}`));
---
+++ example/src/ssr.js
@@ -0,0 +1,25 @@
+const { resolve } = require('path');
+const React = require('react');
+const ReactDOMServer = require('react-dom/server');
+
+if (process.env.NODE_ENV !== 'production') {
+  require('@babel/register')({ extends: resolve(__dirname, '../.babelrc') });
+}
+
+const App = require('./App').default;
+
+module.exports = ({ title }) => {
+  const app = React.createElement(App);
+  const appMarkup = ReactDOMServer.renderToString(app);
+  return `\
+<html>
+<head>
+  <title>${title}</title>
+</head>
+<body>
+  <div id="root">${appMarkup}</div>
+  <script src="client.js" />
+</body>
+</html>
+`;
+};
