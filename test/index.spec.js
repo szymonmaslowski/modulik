1d38cdb8eaf3b5708d6fe3794014cc22606e7b65
Szymon Maslowski <szymon.maslowski@grandparade.co.uk>
2019-07-30 23:55:28 +0200

    Smoke and module config test cases

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,16 +1,114 @@
 const { resolve } = require('path');
-const { existsSync } = require('fs');
+const { existsSync, readFileSync, writeFileSync } = require('fs');
 const rimraf = require('rimraf');
 const watchModule = require('..');
+
+const wait = ms =>
+  new Promise(resolvePromise => {
+    setTimeout(resolvePromise, ms);
+  });
 
 describe('module', () => {
   it('is of function type', () => {
     expect(watchModule).toEqual(expect.any(Function));
   });
-  it('returns object', async done => {
+  it('executes with minimal params with no problems', async done => {
+    const moduleWatched1 = watchModule('./resources/number-module.js');
+    const moduleWatched2 = watchModule({
+      path: './resources/number-module.js',
+    });
+
+    expect(moduleWatched1).toBeTruthy();
+    expect(moduleWatched2).toBeTruthy();
+
+    await moduleWatched1.kill();
+    await moduleWatched2.kill();
+    done();
+  });
+  it('allows for absolute path to module', async done => {
+    const filePath = resolve(__dirname, 'resources/fs-module.txt');
+    const modulePath = resolve(__dirname, 'resources/fs-module.js');
+
+    const moduleWatched1 = watchModule(modulePath);
+    await moduleWatched1.module;
+    expect(existsSync(filePath)).toBeTruthy();
+
+    rimraf.sync(filePath);
+    const moduleWatched2 = watchModule({ path: modulePath });
+    await moduleWatched2.module;
+    expect(existsSync(filePath)).toBeTruthy();
+
+    rimraf.sync(filePath);
+    await moduleWatched1.kill();
+    await moduleWatched2.kill();
+    done();
+  });
+  it('restarts module under provided path on changes to the file', async done => {
+    const filePath = resolve(__dirname, 'resources/fs-module.txt');
+    const modulePath = resolve(__dirname, 'resources/fs-module.js');
+    const moduleContent = readFileSync(modulePath, 'utf-8');
+
+    const moduleWatched = watchModule(modulePath);
+    await moduleWatched.module;
+    rimraf.sync(filePath);
+    writeFileSync(modulePath, `${moduleContent}\n`);
+    await wait(1000);
+    await moduleWatched.module;
+
+    expect(existsSync(filePath)).toBeTruthy();
+
+    rimraf.sync(filePath);
+    await moduleWatched.kill();
+    writeFileSync(modulePath, moduleContent);
+    done();
+  });
+  it('creates object', async done => {
     const moduleWatched = watchModule('./resources/number-module.js');
     expect(moduleWatched).toEqual(expect.any(Object));
     await moduleWatched.kill();
+    done();
+  });
+  it('disables logging when quiet property of config is set to true', async done => {
+    const spy = jest.spyOn(console, 'info');
+    const moduleWatched1 = watchModule('./resources/number-module.js', {
+      quiet: true,
+    });
+    const moduleWatched2 = watchModule({
+      path: './resources/number-module.js',
+      quiet: true,
+    });
+
+    expect(spy).toHaveBeenCalledTimes(0);
+
+    await moduleWatched1.kill();
+    await moduleWatched2.kill();
+    spy.mockRestore();
+    done();
+  });
+  it('disables functionality of watching and restarting when disable property of config is set to true', async done => {
+    const filePath = resolve(__dirname, 'resources/fs-module.txt');
+    const modulePath = resolve(__dirname, 'resources/fs-module.js');
+    const moduleContent = readFileSync(modulePath, 'utf-8');
+
+    const moduleWatched1 = watchModule(modulePath, { disable: true });
+    await moduleWatched1.module;
+    rimraf.sync(filePath);
+    writeFileSync(modulePath, `${moduleContent}\n`);
+    await wait(1000);
+    await moduleWatched1.module;
+
+    expect(existsSync(filePath)).toBeFalsy();
+
+    const moduleWatched2 = watchModule({ path: modulePath, disable: true });
+    await moduleWatched2.module;
+    rimraf.sync(filePath);
+    writeFileSync(modulePath, moduleContent);
+    await moduleWatched1.module;
+
+    expect(existsSync(filePath)).toBeFalsy();
+
+    await moduleWatched1.kill();
+    await moduleWatched2.kill();
     done();
   });
 });
@@ -104,10 +202,7 @@ describe('"restart" property', () => {
     done();
   });
   it('restarts watched module', async done => {
-    const filePath = resolve(
-      __dirname,
-      'resources/fs-module-resources/file.txt',
-    );
+    const filePath = resolve(__dirname, 'resources/fs-module.txt');
 
     rimraf.sync(filePath);
     const moduleWatched = watchModule('./resources/fs-module.js');
--- test/resources/fs-module.js
+++ test/resources/fs-module.js
@@ -1,8 +1,4 @@
 const { resolve } = require('path');
 const { writeFileSync } = require('fs');
 
-writeFileSync(
-  resolve(__dirname, 'fs-module-resources/file.txt'),
-  'content',
-  'utf-8',
-);
+writeFileSync(resolve(__dirname, 'fs-module.txt'), 'content', 'utf-8');
