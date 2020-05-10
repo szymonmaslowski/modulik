b3cbe430102639292f04b5b5f3f7a296e5d6e66b
fredZgred <i.am.szymon.maslowski@gmail.com>
2020-05-10 23:39:57 +0200

    Extract process communication things to separate file

--- src/child.js
+++ src/child.js
@@ -1,3 +1,5 @@
+const { parentController } = require('./bridge');
+
 const modulePath = process.argv[2];
 const childModule = require(modulePath);
 
@@ -10,23 +12,28 @@ try {
   serializable = false;
 }
 
-process.on('message', async ({ type, correlationId, args }) => {
-  if (type === 'invoke') {
-    if (moduleType !== 'function') return;
-    let result = null;
-    try {
-      const data = await childModule(...args);
-      result = { data };
-    } catch (e) {
-      result = { data: e.message, error: true };
-    }
-    process.send({ type: 'invocation-result', correlationId, result });
-  }
-});
-process.send({
-  type: 'ready',
-  data: {
+process.on(
+  'message',
+  parentController.makeMessageHandler({
+    async onInvoke({ correlationId, args }) {
+      if (moduleType !== 'function') return;
+      let result = null;
+      try {
+        const data = await childModule(...args);
+        result = { data };
+      } catch (e) {
+        result = { data: e.message, error: true };
+      }
+      process.send(
+        parentController.invocationResult({ correlationId, result }),
+      );
+    },
+  }),
+);
+
+process.send(
+  parentController.ready({
     type: moduleType,
     body: serializable ? childModule : undefined,
-  },
-});
+  }),
+);
