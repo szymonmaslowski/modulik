2f1262d4ec90cea932a343b72e0e80ce2b028e57
fredZgred <i.am.szymon.maslowski@gmail.com>
2022-04-15 07:48:16 +0200

    Improve the build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,32 @@
 import pluginTypescript from '@rollup/plugin-typescript';
 import glob from 'fast-glob';
+import pluginDts from 'rollup-plugin-dts';
 
-export default {
-  input: glob.sync('modulik/src/!(typings.d).ts'),
-  output: [
-    {
-      dir: 'modulik/lib',
+const external = [
+  'child_process',
+  'chokidar',
+  'events',
+  'path',
+  'uuid',
+  'xstate',
+];
+
+export default [
+  {
+    input: glob.sync('modulik/src/(child|modulik).ts'),
+    output: {
+      dir: `modulik/lib`,
       format: 'cjs',
       exports: 'auto',
+      entryFileNames: '[name].js',
     },
-  ],
-  preserveModules: true,
-  plugins: [pluginTypescript()],
-  external: ['child_process', 'chokidar', 'events', 'path', 'uuid', 'xstate'],
-};
+    plugins: [pluginTypescript()],
+    external,
+  },
+  {
+    input: glob.sync('modulik/src/modulik.ts'),
+    output: [{ file: 'modulik/lib/modulik.d.ts', format: 'es' }],
+    plugins: [pluginDts()],
+    external,
+  },
+];
